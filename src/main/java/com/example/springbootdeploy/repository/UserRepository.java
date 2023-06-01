1d51b47bdd2082c91913b4c168b8af32ec821f69
GMaikerYactayo <maiker.gonzales@vallegrande.edu.pe>
2023-06-01 01:26:51 -0500

    Spring(Security, JPA, Web, DevTools, JWT), PostgreSQL, Swagger

---
+++ src/main/java/com/example/springbootdeploy/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.springbootdeploy.repository;
+
+import com.example.springbootdeploy.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+}
